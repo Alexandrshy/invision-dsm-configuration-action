4a686a1c118de6b393db1b993697817517bcd809
Alexandrshy <alexandrshy@gmail.com>
2020-03-21 22:31:41 +0400

    Deleted irrelevant code

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,13 @@ import * as core from "@actions/core";
 import fs from "fs";
 
 try {
-  const token = core.getInput("token", {
-    required: true
-  });
-
-  console.log("1. token", token);
-  const encodedToken = Buffer.from(token, "base64");
-  console.log("2. encodedToken", encodedToken);
   fs.writeFileSync(
     `${process.env.GITHUB_WORKSPACE}/.dsmrc`,
     JSON.stringify(
       {
-        authToken: token,
+        authToken: core.getInput("token", {
+          required: true
+        }),
         dsmHost: core.getInput("dsmHost"),
         organization: core.getInput("organization", {
           required: true
